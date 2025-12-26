c27dd4d859b274a1a85bb3cf4ec84fbdb0a2ade3
jkh7562 <jkh7562>
2025-12-26 11:11:49 +0900

    hotfix: TailwindCSS 설정 오류 수정

--- main/src/router/root.tsx
+++ main/src/router/root.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { Suspense, lazy } from 'react';
 import { createBrowserRouter } from "react-router-dom";
 
 const Loading = <div>Loading....</div>;
 
 const MainHome = lazy(() => import('../components/MainHome'));
-const RemoteDomainApp = React.lazy(() => import("domain1/DomainApp"));
+const RemoteDomainApp = lazy(() => import("domain1/DomainApp"));
 
 const root = createBrowserRouter([
     { path: "", element: <Suspense fallback={Loading}><MainHome/></Suspense> },
