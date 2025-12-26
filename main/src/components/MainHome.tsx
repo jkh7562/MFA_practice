09f43f176f0b2355ee2e11da68fc11c946a239d8
jkh7562 <jkh7562>
2025-12-26 10:33:28 +0900

    hot-fix: Test용 Consol.log 삭제

--- main/src/components/MainHome.tsx
+++ main/src/components/MainHome.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-console.log("DEBUG: MFA_Practice_Main_URL =", process.env.MFA_Practice_Main_URL);
-console.log("DEBUG: MFA_Practice_Domain1_URL =", process.env.MFA_Practice_Domain1_URL);
-
 export default function MainHome() {
   return <div>This is MainHome!!</div>;
 }
